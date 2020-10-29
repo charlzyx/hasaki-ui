d921009eb760bf9486e62d9d419b3b17ef3bb503
charlzyx <xiaochao.yang@relxtech.com>
2020-10-29 15:14:38 +0800

    feat: fix ws

--- src/views/Watching.vue
+++ src/views/Watching.vue
@@ -26,7 +26,7 @@ export default {
   components: { Navs },
   mounted() {
     const host = window.location.hostname;
-    const socket = new WebSocket("ws://" + host + ":2333/ws");
+    const socket = new WebSocket("ws://" + host + "__hasaki__/ws");
     window.addEventListener('beforeunload', () => {
       socket.send("close");
       socket.onclose = function () {
